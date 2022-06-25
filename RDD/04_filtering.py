9ecb24fea12addf4f86569d64896247527ea4e12
kkote <kyrylo.ktl@gmail.com>
2022-06-25 23:22:56 +0300

    RDD_04 Filtering RDD

---
+++ RDD/04_filtering.py
@@ -0,0 +1,16 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setAppName('Filtering')
+sc = SparkContext.getOrCreate(conf=conf)
+
+rows = sc.textFile('numbers.txt')
+print(rows.collect())
+
+nums = rows.flatMap(str.split).map(int)
+print(nums.collect())
+
+odd_nums = nums.filter(lambda x: x & 1)
+print(odd_nums.collect())
+
+even_nums = nums.filter(lambda x: not x & 1)
+print(even_nums.collect())
