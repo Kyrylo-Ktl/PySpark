0a67cbb8f8b4b36deefa8cfc344391a7b6be9475
kkote <kyrylo.ktl@gmail.com>
2022-06-25 23:01:46 +0300

    RDD_02 Mapping RDD

---
+++ RDD/02_mapping.py
@@ -0,0 +1,14 @@
+from pyspark import SparkConf, SparkContext
+
+
+def split_into_words(row: str) -> list:
+    return [word.lower().removesuffix('.') for word in row.split()]
+
+
+conf = SparkConf().setAppName('Mapping RDD')
+sc = SparkContext.getOrCreate(conf=conf)
+
+rows = sc.textFile('sample.txt')
+
+words = rows.map(split_into_words)
+print(words.collect())
