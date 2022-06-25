3874a481518991503debdc80ca305a1592b1cd3f
kkote <kyrylo.ktl@gmail.com>
2022-06-25 23:04:55 +0300

    RDD_03 Flatmapping RDD

---
+++ RDD/03_flatmap.py
@@ -0,0 +1,14 @@
+from pyspark import SparkConf, SparkContext
+
+
+def split_into_words(row: str) -> list:
+    return [word.removesuffix('.') for word in row.split()]
+
+
+conf = SparkConf().setAppName('Mapping RDD')
+sc = SparkContext.getOrCreate(conf=conf)
+
+rows = sc.textFile('sample.txt')
+
+words = rows.flatMap(split_into_words).map(str.lower)
+print(words.collect())
