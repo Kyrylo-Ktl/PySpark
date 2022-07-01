a53ed2b96f8539ebf2bd06e2ee26bb85e57c36d6
kkote <kyrylo.ktl@gmail.com>
2022-07-01 16:33:33 +0300

    Selecting DF columns

---
+++ DF/05_selecting_columns.py
@@ -0,0 +1,22 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder.appName('Providing schema').getOrCreate()
+
+df = spark.read.options(inferSchema=True, header=True).csv('StudentData.csv')
+df.show()
+
+# name, gender
+df.select('name', 'gender').show()
+
+# name, email
+df.select(df.name, df.email).show()
+
+# roll, name
+df.select(col('roll'), col('name')).show()
+
+# age, gender, name, course, roll, marks, email
+df.select('*').show()
+
+# name, course, roll, marks
+df.select(df.columns[2:6]).show()
