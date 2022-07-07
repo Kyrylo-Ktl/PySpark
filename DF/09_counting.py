97eef0aefe9947ce78e3f61766f9862ba72e9e66
kkote <kyrylo.ktl@gmail.com>
2022-07-07 16:42:22 +0300

    Counting DataFrame rows

---
+++ DF/09_counting.py
@@ -0,0 +1,10 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('Counting').getOrCreate()
+
+df = spark.read.options(inferSchema=True, header=True).csv('StudentData.csv')
+df.show()
+
+print(df.count())
+print(df.filter(df.gender == 'Female').count(), df.filter(df.gender == 'Male').count())
+print(df.filter(df.course == 'DB').count())
