e2a909e438824cd1c79ff5f17c04ff146f4448b0
kkote <kyrylo.ktl@gmail.com>
2022-07-11 18:59:05 +0300

    Sorting and Ordering DataFrame rows

---
+++ DF/12_sorting.py
@@ -0,0 +1,12 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('Sorting').getOrCreate()
+
+df = spark.read.options(inferSchema=True, header=True).csv('StudentData.csv')
+df.show()
+
+df.select(['age', 'course']).distinct().sort(df.age, df.course).show()
+
+df.select(['age', 'course']).distinct().orderBy(df.age, df.course).show()
+
+df.select(['age', 'course']).distinct().sort(df.course.asc(), df.age.desc()).show()
