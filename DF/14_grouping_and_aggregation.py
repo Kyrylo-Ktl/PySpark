e7175a214be52377aa8dd854fa40f1a2a28149f7
kkote <kyrylo.ktl@gmail.com>
2022-07-11 19:13:55 +0300

    Grouping and aggregation DataFrame by several columns

---
+++ DF/14_grouping_and_aggregation.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import sum, avg, max, min, count, round
+
+spark = SparkSession.builder.appName('Grouping and aggregation').getOrCreate()
+
+df = spark.read.options(inferSchema=True, header=True).csv('StudentData.csv')
+df.show()
+
+df.groupBy('course').count().show()
+df.groupBy('course', 'gender').count().show()
+
+df.groupBy('course', 'gender').agg(
+    count('*').alias('total_enrollments'),
+    sum('marks').alias('total_marks'),
+    min('marks').alias('min_mark'),
+    max('marks').alias('max_mark'),
+    round(avg('marks'), 2).alias('avg_mark'),
+).show()
