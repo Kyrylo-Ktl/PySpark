91a4321f3bad4a9268122046762c2f35cdaba0e1
kkote <kyrylo.ktl@gmail.com>
2022-07-11 19:32:50 +0300

    Grouping and filtering DataFrame by several columns

---
+++ DF/15_grouping_and_filtering.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import count, col
+
+spark = SparkSession.builder.appName('Grouping and filtering').getOrCreate()
+
+df = spark.read.options(inferSchema=True, header=True).csv('StudentData.csv')
+df.show()
+
+df.filter(df.gender == 'Male').groupBy('course', 'gender').agg(
+    count('*').alias('total_enrollments')
+).filter(
+    col('total_enrollments') > 85
+).show()
