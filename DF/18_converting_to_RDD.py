02a4bf71eb8db9fc9045465cc3aebbc2558c4616
kkote <kyrylo.ktl@gmail.com>
2022-07-11 20:45:52 +0300

    Converting DataFrame to RDD

---
+++ DF/18_converting_to_RDD.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('Grouping and filtering').getOrCreate()
+
+df = spark.read.options(inferSchema=True, header=True).csv('StudentData.csv')
+df.show()
+
+rdd = df.rdd
+print(type(rdd))
+
+print(rdd.collect())
+
+print(rdd.filter(lambda x: x[0] == 28).collect())
+
+print(rdd.filter(lambda x: x['gender'] == 'Male').collect())
