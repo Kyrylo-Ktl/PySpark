e6728036f1cad83103eb9475e2122889676dce01
kkote <kyrylo.ktl@gmail.com>
2022-07-11 21:39:07 +0300

    Word Count

---
+++ DF/word_count.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, explode, udf
+from pyspark.sql.types import ArrayType, StringType
+
+WORDS_LIST = ArrayType(elementType=StringType(), containsNull=False)
+
+
+@udf(returnType=WORDS_LIST)
+def split_sentence(sentence: str) -> list:
+    return sentence.strip('\n').strip().lower().split()
+
+
+spark = SparkSession.builder.appName('Word Count').getOrCreate()
+rows = spark.read.text('WordData.txt')
+
+words = rows.withColumn('words', explode(split_sentence('value'))).select('words')
+word_count = words.groupBy('words').count().orderBy(col('count').desc())
+word_count.show()
