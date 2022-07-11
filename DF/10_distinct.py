1ed0c8b8992af43b6686199d198c41589983ee8a
kkote <kyrylo.ktl@gmail.com>
2022-07-11 18:11:12 +0300

    Selecting unique rows from DataFrame

---
+++ DF/10_distinct.py
@@ -0,0 +1,9 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('Counting').getOrCreate()
+
+df = spark.read.options(inferSchema=True, header=True).csv('StudentData.csv')
+df.show()
+
+df.select(df.course).distinct().show()
+df.select(df.gender).distinct().show()
